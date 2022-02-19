25dc9216bc90213b752bf1a1c0101f52604ccfa1
Thiago Nobrega <thiagons@live.com>
2022-02-19 18:26:36 -0300

    refatorando req-2 :recycle:

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,20 +3,20 @@ import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
 describe('2. Requisito', () => {
-  it('Verifica se a página contém um h2 com o texto "About Pokédex"', () => {
+  beforeEach(() => {
     render(<About />);
+  });
+  it('Verifica se a página contém um h2 com o texto "About Pokédex"', () => {
     const titleAboutEl = screen.getByRole('heading', { name: /About Pokédex/i });
     expect(titleAboutEl).toBeDefined();
   });
 
   it('Verifica se a página contém dois parágrafos.', () => {
-    render(<About />);
     const paragraphAboutEl = screen.getAllByText(/Pokémons/i);
     expect(paragraphAboutEl).toHaveLength(2);
   });
 
   it('Verifica se a página contém uma imagem de uma Pokédex.', () => {
-    render(<About />);
     const imageAboutEl = screen.getByAltText(/Pokédex/i);
     expect(imageAboutEl.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
